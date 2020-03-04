0aae944542c38b340b7b1c31abad6b51944db53c
Nidhi Verma <59047173+Nidd1995@users.noreply.github.com>
2020-03-04 11:08:37 +0530

    Add files via upload

---
+++ EmailConfig.java
@@ -0,0 +1,31 @@
+package com.kuliza.lending.aramex.lmsportal.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "aws")
+public class EmailConfig {
+
+    private String accessKey;
+
+    private String secretKey;
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
